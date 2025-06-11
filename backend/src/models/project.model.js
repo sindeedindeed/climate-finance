0088b5bf747329f5484566dc39a39c12701ec0c6
Shahabuddin1122 <shavoddin54@gmail.com>
2025-06-11 11:48:44 +0600

    add funding source page insights url

--- backend/src/models/project.model.js
+++ backend/src/models/project.model.js
@@ -11,6 +11,8 @@ Project.addProjectWithRelations = async (data) => {
         const {
             title,
             type,
+            sector,
+            division,
             status,
             approval_fy,
             beginning,
@@ -33,13 +35,13 @@ Project.addProjectWithRelations = async (data) => {
 
         const insertProjectQuery = `
             INSERT INTO Project (
-                project_id, title, type, status, approval_fy, beginning, closing,
+                project_id, title, type, sector, division, status, approval_fy, beginning, closing,
                 total_cost_usd, gef_grant, cofinancing, wash_finance,
                 wash_finance_percent, beneficiaries, objectives
-            ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)
+            ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16)
         `;
         const values = [
-            project_id, title, type, status, approval_fy, beginning, closing,
+            project_id, title, type, sector, division, status, approval_fy, beginning, closing,
             total_cost_usd, gef_grant, cofinancing, wash_finance,
             wash_finance_percent, beneficiaries, objectives
         ];
@@ -142,7 +144,7 @@ Project.getProjectsOverviewStats = async () => {
             SELECT 
                 (SELECT COUNT(*) FROM Project) AS total_projects,
                 (SELECT COUNT(*) FROM Project WHERE now() BETWEEN beginning AND closing) AS active_projects,
-                (SELECT COUNT(*) FROM Project WHERE status IN('Implementated')) AS completed_projects,
+                (SELECT COUNT(*) FROM Project WHERE status IN('Implemented')) AS completed_projects,
                 (SELECT COALESCE(SUM(total_cost_usd), 0) FROM Project) AS total_investment
         `;
 
@@ -150,7 +152,7 @@ Project.getProjectsOverviewStats = async () => {
             SELECT
                 COUNT(*) AS total_projects,
                 COUNT(*) FILTER (WHERE now() BETWEEN beginning AND closing) AS active_projects,
-                    COUNT(*) FILTER (WHERE status IN ('Implementated')) AS completed_projects,
+                    COUNT(*) FILTER (WHERE status IN ('Implemented')) AS completed_projects,
                     COALESCE(SUM(total_cost_usd), 0) AS total_investment
             FROM Project
             WHERE approval_fy = EXTRACT(YEAR FROM CURRENT_DATE)
@@ -185,6 +187,19 @@ Project.getProjectByStatus = async () => {
 
 Project.getProjectBySector = async () => {
     const query = `
+        SELECT sector, COUNT(*) AS value
+        FROM Project
+        GROUP BY sector
+    `;
+    const { rows } = await pool.query(query);
+    return rows.map(row => ({
+        name: row.sector,
+        value: parseInt(row.value)
+    }));
+};
+
+Project.getProjectByType = async () => {
+    const query = `
         SELECT type, COUNT(*) AS value
         FROM Project
         GROUP BY "type"
@@ -212,6 +227,103 @@ Project.getProjectTrend = async () => {
     }));
 };
 
+Project.getFundingSourceOverviewStats = async () => {
+    const client = await pool.connect();
+    try {
+        const query = `
+            SELECT
+                    (SELECT SUM(total_cost_usd) FROM Project) AS total_climate_finance,
+                    (SELECT COUNT(DISTINCT fs.funding_source_id)
+                     FROM Project p
+                              INNER JOIN ProjectFundingSource pfs ON p.project_id = pfs.project_id
+                              INNER JOIN FundingSource fs ON pfs.funding_source_id = fs.funding_source_id
+                     WHERE p.status != 'Implemented'
+                    ) AS active_funding_source,
+                (SELECT COALESCE(SUM(gef_grant), 0) FROM Project) AS committed_funds,
+                (SELECT COALESCE(SUM(disbursement), 0) FROM Project) AS disbursed_funds
+        `;
+
+
+        const trendQuery = `
+            SELECT
+                SUM(p.total_cost_usd) AS total_finance,
+                (
+                    SELECT COUNT(DISTINCT fs.funding_source_id)
+                    FROM Project p2
+                             INNER JOIN ProjectFundingSource pfs ON p2.project_id = pfs.project_id
+                             INNER JOIN FundingSource fs ON pfs.funding_source_id = fs.funding_source_id
+                    WHERE p2.status != 'Implemented'
+                    AND p2.approval_fy = EXTRACT(YEAR FROM CURRENT_DATE)
+                    ) AS active_funding_source,
+                COALESCE(SUM(p.gef_grant), 0) AS committed_funds,
+                COALESCE(SUM(p.disbursement), 0) AS disbursed_funds
+            FROM Project p
+            WHERE p.approval_fy = EXTRACT(YEAR FROM CURRENT_DATE);
+        `;
+
+
+        const result = await client.query(query);
+        const trendResult = await client.query(trendQuery);
+
+        return {
+            ...result.rows[0],
+            current_year: trendResult.rows
+        };
+    } catch (err) {
+        throw err;
+    } finally {
+        client.release();
+    }
+};
+
+Project.getFundingSourceTrend = async () => {
+    const query = `
+        SELECT 
+            approval_fy AS year,
+            SUM(gef_grant) AS gef_grant
+        FROM Project
+        GROUP BY approval_fy
+        ORDER BY approval_fy
+    `;
+    const { rows } = await pool.query(query);
+    return rows.map(row => ({
+        year: row.year.toString(),
+        gef_grant: parseInt(row.gef_grant)
+    }));
+};
+
+Project.getFundingSourceSectorAllocation = async () => {
+    const query = `
+        SELECT 
+            sector AS sector,
+            SUM(gef_grant) AS gef_grant
+        FROM Project
+        GROUP BY sector
+        ORDER BY sector
+    `;
+    const { rows } = await pool.query(query);
+    return rows.map(row => ({
+        sector: row.sector.toString(),
+        gef_grant: parseInt(row.gef_grant)
+    }));
+};
+
+Project.getFundingSource = async () => {
+    const query = `
+        SELECT
+            fs.*,
+            COUNT(DISTINCT p.project_id) AS active_projects,
+            STRING_AGG(DISTINCT p.sector, ', ') AS sectors
+        FROM Project p
+                 INNER JOIN ProjectFundingSource pfs ON p.project_id = pfs.project_id
+                 INNER JOIN FundingSource fs ON pfs.funding_source_id = fs.funding_source_id
+        GROUP BY fs.funding_source_id;
+
+        
+    `;
+    const { rows } = await pool.query(query);
+    return rows;
+};
 
 
 module.exports = Project;
--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {addProject, getAllProjects, updateProject, deleteProject, getProjectById, getProjectsOverviewStats,
-    getProjectByStatus, getProjectBySector, getProjectTrend
+    getProjectByStatus, getProjectBySector, getProjectTrend, getProjectByType, getFundingSourceOverview,
+    getFundingSourceTrend, getFundingSourceSectorAllocation, getFundingSource
 } = require("../controllers/project.controller");
 
 const router = express.Router();
@@ -10,10 +11,17 @@ router.get('/all-project', getAllProjects);
 router.put('/update/:id', updateProject);
 router.delete('/delete/:id', deleteProject);
 router.get('/get/:id', getProjectById);
+//projects
 router.get('/projectsOverviewStats', getProjectsOverviewStats)
 router.get('/get-project-by-status', getProjectByStatus)
 router.get('/get-project-by-sector', getProjectBySector)
 router.get('/get-project-by-trend', getProjectTrend)
+//funding-source
+router.get('/get-project-by-type', getProjectByType)
+router.get('/get-funding-source-overview', getFundingSourceOverview)
+router.get('/get-funding-source-trend', getFundingSourceTrend)
+router.get('/get-funding-source-sector-allocation', getFundingSourceSectorAllocation)
+router.get('/get-funding-source', getFundingSource)
 
 
 
