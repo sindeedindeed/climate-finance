6af2183f48fee9020f4fc98f6685d548aebe6a9e
MAISHAN NADIS <sindeed222@gmail.com>
2025-07-01 16:50:10 +0600

    Update font

--- climate-finance/src/pages/App.jsx
+++ climate-finance/src/pages/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastProvider } from '../components/ui/Toast';
 import { AuthProvider } from '../context/AuthContext';
+import { LanguageProvider } from '../context/LanguageContext';
 import ErrorBoundary from '../components/ui/ErrorBoundary';
 import Routing from '../routing';
 import Navbar from '../components/ui/Navbar';
@@ -12,15 +13,17 @@ function App() {
     <ErrorBoundary>
       <BrowserRouter>
         <AuthProvider>
-          <ToastProvider>
-            <Navbar />
-            <div className="min-h-screen flex flex-col bg-gray-50">
-              <main className="flex-grow">
-                <Routing />
-              </main>
-              <Footer />
-            </div>
-          </ToastProvider>
+          <LanguageProvider>
+            <ToastProvider>
+              <Navbar />
+              <div className="min-h-screen flex flex-col bg-gray-50">
+                <main className="flex-grow">
+                  <Routing />
+                </main>
+                <Footer />
+              </div>
+            </ToastProvider>
+          </LanguageProvider>
         </AuthProvider>
       </BrowserRouter>
     </ErrorBoundary>
